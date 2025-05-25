ccfb1ddf9f6c932b16dc57f52e34735d8de5af08
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2025-05-25 17:26:30 +0000

    feat: create models

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,8 +1,15 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Product
 from flask_admin.contrib.sqla import ModelView
+
+class UserView(ModelView):
+    column_list = ('id', 'email', 'password', 'role')
+
+class ProductView(ModelView):
+    column_list = ('id', 'name', 'description', 'price', 'image_url')
+
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
@@ -11,7 +18,7 @@ def setup_admin(app):
 
     
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
-
+    admin.add_view(UserView(User, db.session))
+    admin.add_view(ProductView(Product, db.session))
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,47 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, Enum
 from sqlalchemy.orm import Mapped, mapped_column
+import enum
 
 db = SQLAlchemy()
+
+class UserRole(str, enum.Enum):
+    ADMIN = "admin"
+    USER = "user"
+
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    role: Mapped["UserRole"] = mapped_column(Enum(UserRole), nullable=False, default=UserRole.USER)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "role": self.role.value,
         }
+
+    def __repr__(self):
+        return self.email
+
+
+class Product(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(600), nullable=False)
+    price: Mapped[float] = mapped_column(nullable=False)
+    image_url: Mapped[str] = mapped_column(String(500), nullable=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "image_url": self.image_url,
+        }
+
+    def __repr__(self):
+        return self.name
